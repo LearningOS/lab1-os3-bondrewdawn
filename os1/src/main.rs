956054eeb67ca7943eeb0b870e1c7973c3b3849a
aucker <aucker2020@gmail.com>
2022-11-15 04:35:41 +0800

    use os1 ref code

--- os1/src/main.rs
+++ os1/src/main.rs
@@ -1,8 +1,29 @@
+//! The main module and entrypoint
+//! 
+//! Various facilities of the kernels are implemented as submodules. The most
+//! important ones are:
+//! 
+//! - [`trap`]: Handles all cases of switching from userspace to the kernel
+//! - [`task`]: Task management
+//! - [`syscall`]: System call handling and implementation
+//! 
+//! The operating system also starts in this module. Kernel code starts
+//! executing from `entry.asm`, after which [`rust_main()`] is called to
+//! initialize various pieces of functionality. (See its source code for
+//! details.)
+//! 
+//! We then call [`task::run_first_task()`] and for the first time go to 
+//! userspace.
+
 #![no_std]
 #![no_main]
 #![feature(panic_info_message)]
+#![feature(alloc_error_handler)]
 
 use log::*;
+#[macro_use]
+extern crate log;
+extern crate alloc;
 
 #[macro_use]
 mod console;
@@ -11,13 +32,19 @@ mod logging;
 mod sbi;
 
 core::arch::global_asm!(include_str!("entry.asm"));
+core::arch::global_asm!(include_str!("link_app.S"));
 
+/// clear BSS segment
 fn clear_bss() {
     extern "C" {
         fn sbss();
         fn ebss();
     }
-    (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
+    // (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
+    unsafe {
+        core::slice::from_raw_parts_mut(sbss as usize as *mut u8, ebss as usize - sbss as usize)
+            .fill(0);
+    }
 }
 
 #[no_mangle]
---
+++ os1/src/syscall/fs.rs
@@ -0,0 +1,18 @@
+//! File and filesystem-related syscalls
+
+const FD_STDOUT: usize = 1;
+
+// Your JOB: TODO: change sys_write() make it pass the test
+pub fn sys_write(fd: usize, buf: *const u8, len: usize) -> isize {
+    match fd {
+        FD_STDOUT => {
+            let slice = unsafe { core::slice::from_raw_parts(buf, len) };
+            let str = core::str::from_utf8(slice).unwrap();
+            print!("{}", str);
+            len as isize
+        }
+        _ => {
+            panic!("Unsupported fd in sys_write!");
+        }
+    }
+}
